a4e629c152b94ef685de21a43df91cc0a7f25ae7
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-11-12 18:41:37 +0900

    Lesson 156: Student Group Management
    Lesson 156: Student Group Management

--- school/resources/views/admin/body/sidebar.blade.php
+++ school/resources/views/admin/body/sidebar.blade.php
@@ -66,7 +66,7 @@
             <ul class="treeview-menu">
               <li><a href="{{ route('student.class.view') }}"><i class="ti-more"></i>Student Class</a></li>
               <li><a href="{{ route('student.year.view') }}"><i class="ti-more"></i>Student Year</a></li>
-              <li><a href="#"><i class="ti-more"></i>Student Group</a></li>
+              <li><a href="{{ route('student.group.view') }}"><i class="ti-more"></i>Student Group</a></li>
               <li><a href="#"><i class="ti-more"></i>Student Shift</a></li>
               <li><a href="#"><i class="ti-more"></i>Fee Category</a></li>
               <li><a href="#"><i class="ti-more"></i>Fee Category Amount</a></li>
