d21a019e8129841fa0e03f5ec80ac575db4d2d58
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-11-14 18:33:56 +0900

    Lesson 179: Working Desingation

--- school/routes/web.php
+++ school/routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Backend\Setup\FeeAmountController;
 use App\Http\Controllers\Backend\Setup\ExamTypeController;
 use App\Http\Controllers\Backend\Setup\SchoolSubjectController;
 use App\Http\Controllers\Backend\Setup\AssignSubjectController;
+use App\Http\Controllers\Backend\Setup\DesignationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -254,6 +255,25 @@ Route::prefix('setups')->group(function(){
     Route::get('assign/subject/details/{class_id}', [AssignSubjectController::class, 'DetailsAssignSubject']
     )->name('assign.subject.details');
 
+    // Designation All Routes 
+
+    Route::get('designation/view', [DesignationController::class, 'ViewDesignation']
+    )->name('designation.view');
+
+    Route::get('designation/add', [DesignationController::class, 'DesignationAdd']
+    )->name('designation.add');
+
+    Route::post('designation/store', [DesignationController::class, 'DesignationStore']
+    )->name('store.designation');
+
+    Route::get('designation/edit/{id}', [DesignationController::class, 'DesignationEdit']
+    )->name('designation.edit');
+
+    Route::post('designation/update/{id}', [DesignationController::class, 'DesignationUpdate']
+    )->name('update.designation');
+
+    Route::get('designation/delete/{id}', [DesignationController::class, 'DesignationDelete']
+    )->name('designation.delete');
 
 });
 
