0e7286ebf1b89e9954a2bb7210b63eb9c004e6ee
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-11-20 23:12:17 +0900

    Lesson 282: Student ID Card
    Lesson 282: Student ID Card

--- school/resources/views/admin/body/sidebar.blade.php
+++ school/resources/views/admin/body/sidebar.blade.php
@@ -152,8 +152,8 @@
             <li class="{{ ($route == 'monthly.profit.view')?'active':'' }}"><a href="{{ route('monthly.profit.view') }}"><i class="ti-more"></i>Monthly-Yearly Profite</a></li>
             <li class="{{ ($route == 'marksheet.generate.view')?'active':'' }}"><a href="{{ route('marksheet.generate.view') }}"><i class="ti-more"></i>MarkSheet Generate</a></li>
             <li class="{{ ($route == 'attendance.report.view')?'active':'' }}"><a href="{{ route('attendance.report.view') }}"><i class="ti-more"></i>Attendance Report</a></li>
-            <li class="#"><a href="#"><i class="ti-more"></i>Student Result </a></li>
-            <li class="#"><a href="#"><i class="ti-more"></i>Student ID Card </a></li>             
+            <li class="{{ ($route == 'student.result.view')?'active':'' }}"><a href="{{ route('student.result.view') }}"><i class="ti-more"></i>Student Result </a></li>
+            <li class="{{ ($route == 'student.idcard.view')?'active':'' }}"><a href="{{ route('student.idcard.view') }}"><i class="ti-more"></i>Student ID Card </a></li>             
           </ul>
         </li>
 		
